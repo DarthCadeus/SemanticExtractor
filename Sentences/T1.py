8751fd6cb24de358ee77a043d1b91a46b72491b7
Bingdong Li <lbdro2017@yandex.com>
2018-08-01 14:56:48 +0800

    Tier 1.5

--- Sentences/T1.py
+++ Sentences/T1.py
@@ -166,7 +166,7 @@ def extract_3(tagged_corpus):
     return sbj_dt, sbj_att, sbj_adt, sbj, pdt, obj_dt, obj_att, obj_adt, obj
 
 
-# Tier 1.3
+# Tier 1.4
 def extract_4(tagged_corpus):
     global TIER
     TIER = 1.4
@@ -266,13 +266,123 @@ def extract_4(tagged_corpus):
             sbj_toi, obj_toi
 
 
-corpus = "He tried to escape"
+# Tier 1.5
+def extract_5(tagged_corpus):
+    global TIER
+    TIER = 1.5
+    sbj_toi = False
+    sbj_dt = None
+    sbj_att = []
+    sbj_adt = []
+    sbj = None
+    sbj_tpa = []  # to-infinitive post adverbs
+    pdt = None
+    obj_toi = False
+    obj_dt = None
+    obj_att = []
+    obj_adt = []
+    obj = None
+    obj_tpa = []
+    for word in tagged_corpus:
+        if word[0].lower() == "to":
+            print(sbj, pdt, obj)
+            if sbj and pdt and not obj:
+                obj_toi = True
+            elif not sbj:
+                sbj_toi = True
+        if word[1] == "DT":
+            if pdt and not obj_toi:
+                obj_dt = word
+            elif not sbj_toi:
+                sbj_dt = word
+        if NLP.Converter.penn_to_wn(word[1]) == "r":
+            if pdt:
+                if not obj_toi:
+                    if obj_att:
+                        closest = obj_att[len(obj_att)-1]
+                        if closest[1] is None:
+                            closest[0].append(word)
+                        else:
+                            obj_att.append([[word], None])
+                    else:
+                        obj_att.append([[word], None])
+                else:
+                    if obj:
+                        obj_tpa.append(word)
+            else:
+                if not sbj_toi:
+                    if sbj_att:
+                        closest = sbj_att[len(sbj_att)-1]
+                        if closest[1] is None:
+                            closest[0].append(word)
+                        else:
+                            sbj_att.append([[word], None])
+                    else:
+                        sbj_att.append([[word], None])
+                else:
+                    if sbj:
+                        sbj_tpa.append(word)
+        if NLP.Converter.penn_to_wn(word[1]) == "a" or word[1] == "PRP$":
+            if pdt and not obj_toi:
+                if obj_att:
+                    closest = obj_att[len(obj_att)-1]
+                    if closest[1] is None:
+                        closest[1] = word
+                    else:
+                        obj_att.append([[], word])
+                else:
+                    obj_att.append([[], word])
+            elif not sbj_toi:
+                if sbj_att:
+                    closest = sbj_att[len(sbj_att)-1]
+                    if closest[1] is None:
+                        closest[1] = word
+                    else:
+                        sbj_att.append([[], word])
+                else:
+                    sbj_att.append([[], word])
+        if NLP.Converter.penn_to_wn(word[1]) == "n" or \
+                word[1] == "PRP" or word[0].lower() == "it" or \
+                word[1] == "VBG" or \
+                NLP.Determiner.is_gerund(word[0]):
+            # subjects have to appear before objects
+            if NLP.Converter.penn_to_wn(word[1]) == "n":
+                if not pdt and not sbj_toi:
+                    if sbj:
+                        sbj_adt.append(sbj)
+                        sbj = word
+                    else:
+                        sbj = word
+                elif not obj_toi:
+                    if obj:
+                        obj_adt.append(obj)
+                        obj = word
+                    else:
+                        obj = word
+            else:
+                if pdt and not obj_toi:
+                    obj = word
+                elif not sbj_toi:
+                    sbj = word
+        if NLP.Converter.penn_to_wn(word[1]) == "v" and \
+                word[1] != "VBG":
+            if sbj_toi and not sbj:
+                sbj = word
+            elif not pdt:
+                pdt = word  # the all important line
+            elif obj_toi and not obj:
+                obj = word
+    return sbj_dt, sbj_att, sbj_adt, sbj, sbj_tpa, pdt, obj_dt, obj_att, obj_adt, obj, \
+            obj_tpa, sbj_toi, obj_toi
+
+
+corpus = "To escape too quickly is death"
 tagged = NLP.Basic(st).tag(corpus)
 
 if __name__ == '__main__':
     print("TAGGED:", tagged)
     start_time = time.time()
-    extracted = extract_4(tagged)
+    extracted = extract_5(tagged)
     print(extracted)
     graph = to_graph.to_graph(extracted, TIER, True)
     print(graph.number_of_nodes())
--- Sentences/to_graph.py
+++ Sentences/to_graph.py
@@ -42,13 +42,15 @@ def to_graph(extracted, tier=None, display_opt=False):
         else:
             obj = obj[0]
         graph.add_edge(sbj, obj, object=pdt)
-    elif tier == 1.3:
+    elif tier == 1.3 or tier == 1.4:  # basically the same
         sbj = extracted[3]
         if not display_opt:
             sbj = EC.Entity(sbj[0], tag=sbj[1])
             sbj.dt = extracted[0]
             sbj.all = extracted[1]
             sbj.adt = extracted[2]
+            if tier == 1.4:
+                sbj.toi = extracted[13]  # BUG: problems with index, deprecated anyway but had better fix
             for attr in extracted[1]:
                 setattr(sbj, attr[0], None)  # leave this empty for now ...
         else:
@@ -63,6 +65,38 @@ def to_graph(extracted, tier=None, display_opt=False):
                 obj.dt = extracted[9]
                 obj.all = extracted[10]
                 obj.adt = extracted[11]
+                if tier == 1.4:
+                    obj.toi = extracted[14]
+                for attr in extracted[4]:
+                    setattr(obj, attr[0], None)
+            else:
+                obj = obj[0]
+        graph.add_edge(sbj, obj, object=pdt)
+    elif tier == 1.5:
+        sbj = extracted[3]
+        if not display_opt:
+            sbj = EC.Entity(sbj[0], tag=sbj[1])
+            sbj.dt = extracted[0]
+            sbj.all = extracted[1]
+            sbj.adt = extracted[2]
+            sbj.toi = extracted[11]
+            sbj.tpa = extracted[4]
+            for attr in extracted[1]:
+                setattr(sbj, attr[0], None)
+        else:
+            sbj = sbj[0]
+        pdt = extracted[5]
+        if display_opt:
+            pdt = pdt[0]
+        obj = guard(extracted[9])
+        if obj != nil:
+            if not display_opt:
+                obj = EC.Entity(obj[0], tag=obj[1])
+                obj.dt = extracted[6]
+                obj.all = extracted[7]
+                obj.adt = extracted[8]
+                obj.toi = extracted[16]
+                obj.tpa = extracted[12]
                 for attr in extracted[4]:
                     setattr(obj, attr[0], None)
             else:
